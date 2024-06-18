a8d7a8d1f569eb08906618b7938bafbde30bcef4
lyytaw <lyytaw@gmail.com>
2024-06-18 18:23:50 +0800

    config

--- doc_query/vector_tool/merge.py
+++ doc_query/vector_tool/merge.py
@@ -6,15 +6,30 @@
 import os.path
 import shutil
 
+import numpy as np
 from langchain_community.vectorstores import FAISS
+from langchain_community.vectorstores.faiss import dependable_faiss_import
 
 from doc_query.common.utils import get_file_list, get_vector_index_name, get_logger
 
 logger = get_logger()
 
 
+def merge_faiss(origin, need):
+    vector1 = np.array(origin.index.reconstruct_n(0, origin.index.ntotal))
+    vector2 = np.array(origin.index.reconstruct_n(0, need.index.ntotal))
+
+    merged_vectors = np.vstack((vector1, vector2))
+    faiss = dependable_faiss_import()
+    new_index = faiss.IndexFlatL2(merged_vectors.shape[1])
+
+    new_index.add(merged_vectors)
+    vector1.index = new_index
+
+
 def backup_vector_db(vector_db):
     backup_file = vector_db + "_backup"
+    shutil.rmtree(backup_file)
     shutil.copytree(vector_db, backup_file)
 
 
@@ -27,8 +42,9 @@ def merged_vector_db(need_merged_path, origin_db_path, embeddings):
         origin_db = FAISS.load_local(origin_db_path, embeddings, index_name=get_vector_index_name(),
                                      allow_dangerous_deserialization=True
                                      )
-        origin_db.merge_from(need_merged_db)
-        origin_db.save_local(need_merged_path, origin_db_path)
+        merge_faiss(origin_db_path, need_merged_db)
+        # origin_db.merge_from(need_merged_db)
+        origin_db.save_local(origin_db_path, get_vector_index_name())
     else:
         shutil.copytree(need_merged_path, origin_db_path)
 
@@ -41,7 +57,7 @@ def merge_vector_db_main(origin_db_path, need_merged_db_path, embeddings):
     for product in need_merged_product_list:
         product_path = os.path.join(need_merged_db_path, product)
 
-        origin_product_db_path = os.path.join(origin_db_path, product_path)
+        origin_product_db_path = os.path.join(origin_db_path, product)
         print(f"origin db path {origin_product_db_path}")
 
         for file_name in get_file_list(product_path):
--- run.sh
+++ run.sh
@@ -2,3 +2,5 @@ cd doc_query
 export PYTHONPATH=/mnt/workspace/doc_chatbot/
 
 python vectordb.py init
+
+python vectordb.py merge
