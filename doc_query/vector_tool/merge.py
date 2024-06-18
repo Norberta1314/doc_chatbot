3a673713802c6697a271507583b210e95e108399
lyytaw <lyytaw@gmail.com>
2024-06-18 17:36:39 +0800

    config

--- doc_query/vector_tool/merge.py
+++ doc_query/vector_tool/merge.py
@@ -19,10 +19,14 @@ def backup_vector_db(vector_db):
 
 
 def merged_vector_db(need_merged_path, origin_db_path, embeddings):
-    need_merged_db = FAISS.load_local(need_merged_path, embeddings, index_name=get_vector_index_name())
+    print(f"merged, need merged path:{need_merged_path}, origin: {origin_db_path}")
+    need_merged_db = FAISS.load_local(need_merged_path, embeddings, index_name=get_vector_index_name(),
+                                      allow_dangerous_deserialization=True)
     logger.info("there are %s ids need to be merged", str(len(need_merged_db.index_to_docstore_id)))
     if os.path.exists(origin_db_path):
-        origin_db = FAISS.load_local(origin_db_path, embeddings, index_name=get_vector_index_name())
+        origin_db = FAISS.load_local(origin_db_path, embeddings, index_name=get_vector_index_name(),
+                                     allow_dangerous_deserialization=True
+                                     )
         origin_db.merge_from(need_merged_db)
         origin_db.save_local(need_merged_path, origin_db_path)
     else:
@@ -38,6 +42,7 @@ def merge_vector_db_main(origin_db_path, need_merged_db_path, embeddings):
         product_path = os.path.join(need_merged_db_path, product)
 
         origin_product_db_path = os.path.join(origin_db_path, product_path)
+        print(f"origin db path {origin_product_db_path}")
 
         for file_name in get_file_list(product_path):
             need_merged_product = os.path.join(product_path, file_name)
