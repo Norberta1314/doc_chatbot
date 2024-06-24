7ec66e74563a2bbf82b43e6cf2c61db6fe060546
lyytaw <lyytaw@gmail.com>
2024-06-24 18:33:40 +0800

    自动化merge代码

--- merge.sh
+++ merge.sh
@@ -6,11 +6,11 @@ mkdir vectordb
 cd doc_query
 python vectordb.py merge vectordb total_doc
 cd ../
-rm -rf vector_all_db/*
-mkdir -p vector_all_db/all
+rm -rf vector_all_db
+mkdir -p vector_all_db
 
 cp -r vectordb vector_all_db/all
 cd doc_query
 python vectordb.py merge vectordb vector_all_db
 cd ../
-cp -r vectordb doc_chatbot/vectordb
+cp -r vectordb doc_query
